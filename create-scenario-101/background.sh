83f3b84f3066066a23725d9af5086c8cadc811be
Alessandro Pironi <55445310+alepironi@users.noreply.github.com>
2021-03-01 18:06:07 +0100

    Update background.sh

--- create-scenario-101/background.sh
+++ create-scenario-101/background.sh
@@ -2,4 +2,6 @@ export PS1="\e[1;32m[\u@\h] \w $ \e[m"
 cd /tmp
 mkdir my_great_app 
 cd my_great_app
+git config user.email alessandro.pironi@capgemini.com
+git config user.name "Alessandro Pironi"
 clear
