96ffdf747dae5badcfe2d05cf418a3f02339e211
Alessandro Pironi <alessandro.pironi@capgemini.com>
2021-03-02 14:35:30 +0100

    bashrc

--- remote repo/background.sh
+++ remote repo/background.sh
@@ -1,6 +1,4 @@
-touch /tmp/test
 echo "PS1=\"\e[1;32m[\u@\h] \w $ \e[m\"" >> /root/.bashrc
-export PS1="\e[1;32m[\u@\h] \w $ \e[m"
 git config --global user.email alessandro.pironi@capgemini.com
 git config --global user.name "Alessandro Pironi"
 cd /tmp
