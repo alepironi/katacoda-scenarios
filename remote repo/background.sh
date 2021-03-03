d675e3c1a0ba3a4a71e3f40305a563d3172822e4
Alessandro Pironi <alessandro.pironi@capgemini.com>
2021-03-03 19:44:20 +0100

    password

--- remote repo/background.sh
+++ remote repo/background.sh
@@ -2,7 +2,6 @@ echo "export PS1=\"\e[1;32m[\w] $ \e[m\"" >> /root/.bashrc
 . .bashrc
 git config --global user.email pironi.alessandro@gmail.com
 git config --global user.name "alepironi"
-git config --global user.password "3a65f8586f246afb85327bf692d689d3598daa06"
 mkdir /tmp/projects/
 mkdir /tmp/projects/developer1
 mkdir /tmp/projects/developer2
