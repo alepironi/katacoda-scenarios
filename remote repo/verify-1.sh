27d769d8e85da4801a3a0d4378abd3836aec8c14
Alessandro Pironi <alessandro.pironi@capgemini.com>
2021-03-04 15:32:49 +0100

    bash

--- remote repo/verify-1.sh
+++ remote repo/verify-1.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-if [ ! -f /tmp/workspace/developer1/myproject/myfile.txt ] ; then
-	touch /tmp/ko.txt
-    exit 1
-fi
-touch /tmp/ok.txt
-exit 0
+[ -d /tmp/workspace/developer1/myproject/myfile.txt ] && echo "done"
