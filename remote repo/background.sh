3c78d445b076b89e97b2946bada40d972fcfa4d1
Alessandro Pironi <alessandro.pironi@capgemini.com>
2021-03-04 14:39:50 +0100

    courseData

--- remote repo/background.sh
+++ remote repo/background.sh
@@ -1,4 +1,5 @@
 echo "export PS1=\"\e[1;32m[\w] $ \e[m\"" >> /root/.bashrc
+. .bashrc
 git config --global user.email pironi.alessandro@gmail.com
 git config --global user.name "alepironi"
 
@@ -15,4 +16,6 @@ git config core.bare true
 mkdir /tmp/workspace/
 mkdir /tmp/workspace/developer1
 mkdir /tmp/workspace/developer2
+
+cd /tmp/workspace/developer1
 clear
