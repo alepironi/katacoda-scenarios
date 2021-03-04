65052fe5bab29118c2d09529c222b2ef4df0d732
Alessandro Pironi <alessandro.pironi@capgemini.com>
2021-03-04 15:14:54 +0100

    verify

--- remote repo/verify-1.sh
+++ remote repo/verify-1.sh
@@ -1,5 +1,3 @@
-FILE=/tmp/workspace/developer1/myproject/myfile.txt
-if [ -f "$FILE" ]; then
-    exit 0
+if [[ ! -f /tmp/workspace/developer1/myproject/myfile.txt ]] ; then
+    exit -1
 fi
-exit -1
